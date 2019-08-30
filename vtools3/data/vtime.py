e9165d0fbbed02334553b2dd6ba7530c8e6c121b
Eli Ateljevich <eli@water.ca.gov>
2019-08-30 11:26:17 -0700

    Added a function to convert pandas tz-unaware dataframes from local time (including summer DST) to standard time. The standard time specification is still not something I fully understand, but I found defaults that work in the case of the PST/PDT to PST conversion.

--- vtools3/data/vtime.py
+++ vtools3/data/vtime.py
@@ -53,7 +53,46 @@ def years(y):
 
 
 
+
+def dst_to_standard_naive(ts, dst_zone = "US/Pacific", standard_zone="Etc/GMT+8"):
+    """ Convert timezone-unaware series from a local (with daylight) time to standard time
+        This would be useful, say, for converting a series that is PDT during summer to one that is not.
+        The routine is mainly to treat cases where the time stamps at DST interfaces are not redundant -- if they 
+        are you can probably use tz_convert and tz_localize with the ambiguous = 'infer' option and do the job more 
+        efficiently, but lots of databases don't store data this way.
+        
+        The choice of the standard_zone is, it seems, buggy. The defaults are supposed to convert from PST/PDT to pure PST,
+        and the latter should be GMT-8. In a sense, this function is included before the behavior is really understood.
+        
+        Only regular series are accepted ... this is a quirk of the implementation
+        """
+
+
+    # Create a date range that spans the possible times and is same refinement
+    try:
+        dt = ts.freq 
+    except:
+        dt = pd.offsets.Minute(15)
+    hr = pd.offsets.Hour(1)
+    ndx2 = pd.date_range(start=ts.index[0] - hr, end=ts.index[-1] + hr,freq=dt,tz=dst_zone)
+
+    # Here is the determination of whether it is dst
+    isdst = [bool(x.dst()) for x in ndx2.to_pydatetime()]
+
+    # Use DataFrame indexing to perform the lookup for values in my original index
     
+
+    df2 = pd.DataFrame({"isdst":isdst},index=ndx2.tz_localize(None))    
+    df2 = df2.loc[~df2.index.duplicated(keep="last"),:]
+
+    print ts.index
+    print df2
+    ambig = df2.loc[ts.index,"isdst"].values
+
+    # Here is the real work        
+    ts2 = ts.tz_localize("US/Pacific",ambiguous=ambig).tz_convert(standard_zone).tz_localize(None) 
+    return ts2    
+        
     
     
     
