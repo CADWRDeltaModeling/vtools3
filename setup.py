e56d0c9709bcfa2b55376e7c6307f9affa3ed1af
Nicky Sandhu <psandhu@water.ca.gov>
2019-11-05 15:35:24 -0800

    added conda recipe with versioning

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+
+##------------------ VERSIONING BEST PRACTICES --------------------------##
+import os
+import re
+import codecs
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = ["numpy>=1.16,<2","pandas>=0.23"]
+
+setup(
+    name='vtools3',
+    version=find_version("vtools", "__init__.py"),
+    description="Timeseries analysis and processing tools using pandas/xarray",
+    license="Apache",
+    author="Eli Ateljevich",
+    author_email='eli@water.ca.gov',
+    url='https://github.com/CADWRDeltaModeling/vtools3',
+    packages=['vtools'],
+    install_requires=requirements,
+    keywords='vtools3',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
---
+++ vtools/__init__.py
@@ -0,0 +1,6 @@
+
+"""Top-level package for vtools"""
+
+__author__ = """Eli Ateljevich"""
+__email__ = 'eli@water.ca.gov'
+__version__ = '3.0.0'
