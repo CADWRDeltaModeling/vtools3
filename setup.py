d0a387d879482e2edbd01a27f58c9b602de93f80
Nicky Sandhu <psandhu@water.ca.gov>
2019-11-07 16:46:11 -0800

    adding scipy dependency and fix to include all submodules

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 ##------------------ VERSIONING BEST PRACTICES --------------------------##
 import os
@@ -24,19 +24,21 @@ def find_version(*file_paths):
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-requirements = ["numpy>=1.16,<2","pandas>=0.23"]
+requirements = ["numpy>=1.16,<2", "pandas>=0.23", "scipy>=1.2"]
 
 setup(
     name='vtools3',
     version=find_version("vtools", "__init__.py"),
     description="Timeseries analysis and processing tools using pandas/xarray",
     license="Apache",
+    long_description=readme,
+    install_requires=requirements,
+    include_package_data=True,
+    keywords='vtools',
+    packages=find_packages(),
     author="Eli Ateljevich",
     author_email='eli@water.ca.gov',
     url='https://github.com/CADWRDeltaModeling/vtools3',
-    packages=['vtools'],
-    install_requires=requirements,
-    keywords='vtools3',
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
