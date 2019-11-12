f01add21a329735a10d7d42182950d8055fe6883
Kijin Nam <knam@water.ca.gov>
2019-11-12 09:04:57 -0800

    Update for CI
    Move test files and add path information for CI

--- tests/test_godin_filter.py
+++ tests/test_godin_filter.py
@@ -1,6 +1,8 @@
+import os
 import pandas as pd
 import vtools.functions.filter as filter
 import pytest
+
 
 def test_godin():
     '''
@@ -9,9 +11,15 @@ def test_godin():
     The godin filtering of that series yields the coefficients that are being used
     This is compared with the vtools result based on the same.
     '''
-    ts=pd.read_csv('godintest1.csv', parse_dates=True, index_col=0)
-    ts.index.freq=ts.index.inferred_freq #FIXME: better way to do this on parse?
-    tsg=filter.godin_filter(ts)
-    tsg_vtools=pd.read_csv('godintest-vtools.csv', parse_dates=True, index_col=0)
-    tsg_vtools.index.freq=tsg_vtools.index.inferred_freq
-    pytest.approx(tsg_vtools['05JAN1990':'15FEB1990'].values,tsg['05JAN1990':'15FEB1990'].values)
+    fname_input = os.path.join(os.path.dirname(__file__),
+                               'test_data/godintest1.csv')
+    ts = pd.read_csv(fname_input, parse_dates=True, index_col=0)
+    # FIXME: better way to do this on parse?
+    ts.index.freq = ts.index.inferred_freq
+    tsg = filter.godin_filter(ts)
+    fname_expected = os.path.join(os.path.dirname(__file__),
+                                  'test_data/godintest-vtools.csv')
+    tsg_vtools = pd.read_csv(fname_expected, parse_dates=True, index_col=0)
+    tsg_vtools.index.freq = tsg_vtools.index.inferred_freq
+    pytest.approx(tsg_vtools['05JAN1990':'15FEB1990'].values,
+                  tsg['05JAN1990':'15FEB1990'].values)
