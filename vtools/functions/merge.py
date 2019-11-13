1db6e3e47b88fd93f15987b1905600ecc0e73cd1
Kijin Nam <knam@water.ca.gov>
2019-11-13 09:52:14 -0800

    Lint files

--- vtools/functions/merge.py
+++ vtools/functions/merge.py
@@ -4,7 +4,7 @@ import pandas as pd
 def ts_merge(series):
     """ merge a number of timeseries together and return a new ts.
     Similar to concatenate, but provides more control over order in cases of overlap
-    
+
     Parameters
     ----------
     series  :  tuple(:class:`DataFrame <pandas:pandas.DataFrame>`) or tuple(:class:`DataArray) <xarray:xarray.DataArray>`
@@ -15,16 +15,15 @@ def ts_merge(series):
         A new time series with time interval same as the inputs, time extent
         the union of the inputs, and filled first with ts1, then with remaining
         gaps filled with ts2, then ts3....
-        
+
     """
 
     # this concatenates, leaving redundant indices in df0, df1, df2
-    dfmerge = pd.concat(series,sort=True)
+    dfmerge = pd.concat(series, sort=True)
 
     # finally, drop duplicate indices
-    dfmerge = dfmerge.loc[~dfmerge.index.duplicated(keep='last')]    
+    dfmerge = dfmerge.loc[~dfmerge.index.duplicated(keep='last')]
     # Now apply, in priority order, each of the original dataframes to fill the original
     for ddf in series:
         dfmerge = dfmerge.combine_first(ddf)
     return dfmerge
-
