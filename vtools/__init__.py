a4c11447e811133879eac513edc0331951c771fe
Nicky Sandhu <psandhu@water.ca.gov>
2019-12-20 14:28:32 -0800

    incrementing fix version and adding author names

--- vtools/__init__.py
+++ vtools/__init__.py
@@ -1,6 +1,6 @@
 
 """Top-level package for vtools"""
 
-__author__ = """Eli Ateljevich"""
+__author__ = """Eli Ateljevich, Nicky Sandhu, Kijin Nam"""
 __email__ = 'eli@water.ca.gov'
-__version__ = '3.0.0'
+__version__ = '3.0.1'
