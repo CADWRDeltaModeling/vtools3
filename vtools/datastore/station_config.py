475a49ffb04c6604ea6043f96b2c2b3d5010e33d
Eli <Eli.Ateljevich@water.ca.gov>
2021-07-27 17:20:22 -0700

    Changes to accomodate DES downloading.

--- vtools/datastore/station_config.py
+++ vtools/datastore/station_config.py
@@ -2,14 +2,14 @@ import yaml
 import os
 
 config = None    
-
+localdir = os.path.split(__file__)[0]
     
-with open("station_config.yaml", 'r') as stream:
+with open(os.path.join(localdir,"station_config.yaml"), 'r') as stream:
     config = yaml.load(stream,Loader=yaml.FullLoader)
     
 def config_file(label):
     fname = config[label]
-    localdir = os.path.split(__file__)[0]
+    
     # is path in local directory?
     localpath = os.path.join(localdir,fname)
     if os.path.exists(localpath):  
