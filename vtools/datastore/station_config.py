dcc5376cb7d860a0689cc46fc9937ae9abccda0a
Eli <Eli.Ateljevich@water.ca.gov>
2021-09-25 16:57:08 -0700

    Added functionality to retrieve location of config file

--- vtools/datastore/station_config.py
+++ vtools/datastore/station_config.py
@@ -6,7 +6,12 @@ localdir = os.path.split(__file__)[0]
     
 with open(os.path.join(localdir,"station_config.yaml"), 'r') as stream:
     config = yaml.load(stream,Loader=yaml.FullLoader)
-    
+
+def configuration():
+    config_ret = config.copy()
+    config_ret["config_file_location"] = __file__
+    return config_ret
+
 def config_file(label):
     fname = config[label]
     
