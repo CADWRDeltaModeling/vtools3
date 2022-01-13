cde63a870fa169a9ec4f086c8077c502b0c1e50b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-14 00:32:59 +0800

    Delete setup_devenv.bat

--- setup_devenv.bat
+++
@@ -1,6 +0,0 @@
-conda create -y -n dev_vtools3
-conda install -y -n dev_vtools3 -c cadwr-dms -c defaults numpy pandas scipy beautifulsoup4 xlrd
-# for dev environment
-conda install -y -c cadwr-dms -c defaults -c conda-forge -n dev_vtools3 pytest pytest-runner versioneer
-# for docs generation
-conda install -y -c cadwr-dms -c defaults -c conda-forge -n dev_vtools3 sphinx nbsphinx matplotlib sphinx-argparse numpydoc
