718a741fe07bb2150f3fafe9fc2c0010333b21d5
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-14 00:22:21 +0800

    Delete vtools directory

--- vtools/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Top-level package for vtools"""
-
-__author__ = """Eli Ateljevich, Nicky Sandhu, Kijin Nam"""
-__email__ = 'eli@water.ca.gov'
-__version__ = '3.0.4'
-
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
--- vtools/datastore/__init__.py
+++
@@ -1,5 +0,0 @@
-from .download_nwis import nwis_download
-from .download_noaa import noaa_download
-from .download_wdl import wdl_download
-from .download_cdec import cdec_download
-from .read_ts import read_ts
