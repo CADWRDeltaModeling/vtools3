cfdc09adda7d6d7b5b0b38724ff5ef4c6ba790f3
Eli <Eli.Ateljevich@water.ca.gov>
2022-05-18 14:25:21 -0700

    Added function to read and provide station_dbase

--- vtools/datastore/station_config.py
+++ vtools/datastore/station_config.py
@@ -1,11 +1,29 @@
 import yaml
 import os
+import pandas as pd
 
 config = None    
 localdir = os.path.split(__file__)[0]
     
 with open(os.path.join(localdir,"station_config.yaml"), 'r') as stream:
     config = yaml.load(stream,Loader=yaml.FullLoader)
+
+
+def station_dbase(dbase_name=None):
+    if dbase_name is None:
+        dbase_name = config_file("station_dbase")
+    db = pd.read_csv(dbase_name,sep=",",comment="#",header=0,index_col="id",dtype={"agency_id":str})
+    db["agency_id"] = db["agency_id"].str.replace("\'","",regex=True)
+    
+    dup = db.index.duplicated()
+    db.index = db.index.str.replace("'","")
+    if dup.sum(axis=0)> 0:
+        print("Duplicates")
+        print(db[dup])
+        raise ValueError("Station database has duplicate id keys. See above")
+    return db
+
+
 
 def configuration():
     config_ret = config.copy()
