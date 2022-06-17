18e0d43f19df38066a717e603a22cee1b243505c
Shu <Qiang.Shu@water.ca.gov>
2022-06-16 21:55:13 -0700

    add unit impulse response method

--- vtools/functions/frequency_response.py
+++ vtools/functions/frequency_response.py
@@ -20,7 +20,8 @@ def compare_response(cutoff_period):
         The comparison includes, squared cosine-Lanczos, Godin and boxcar.
     """
     interval=0.25 # sample interval 15min as ratio of one hour
-    cf=2.0*interval/cutoff_period #cuf off frequency as ratio of Nyquist frequency
+    
+    cf=2.0*interval/cutoff_period #cufoff frequency as ratio of Nyquist frequency
     
     ## C_L of size 70hours
     m0=int(70.0/interval) # cosine lanczos filer window size as number of smaples, here use size of 70hours
@@ -81,9 +82,69 @@ def compare_response(cutoff_period):
     plt.grid(b=True, which='both', color='0.9', linestyle='-', linewidth=0.5)
     plt.tight_layout()
     ax.legend(loc="lower right")
+    
+def unit_impulse_ts(size,interval):
+    """
+    Parameters
+    ----------
+    size : int
+        Length of impluse time series, odd number.
+    interval : string
+        time series interval, such as "15min"
 
+    Returns
+    -------
+    pandas.dataframe
+        time sereies value 0.0 except 1 at the middle of time series.
+
+    """
+    idx=pd.date_range("2001-1-1",periods=size,freq=interval)
+    val=np.zeros(size)
+    mid_idx=int(size/2)
+    val[mid_idx]=1.0
+    return pd.Series(val,index=idx)
+    
+
+def compare_response2(unit_impulse,cutoff_period):
+    
+    fig = plt.figure(figsize=(6,6),dpi=300)
+    ax = fig.add_subplot(1,1,1)
+    ax.set_ylim(-.2,1.5)
+    worN=4096
+    dt=unit_impulse.index[1]-unit_impulse.index[0]
+    cf=2.0*dt.seconds/(cutoff_period*3600) #cuf off frequency as ratio of Nyquist frequency
+    dt_hours_ratio=dt.seconds/3600.0
+    ## set consine_lanczos as 70h
+    cl_size_hours1=70
+    cl_size1=int(cl_size_hours1*3600/dt.seconds)
+    ##  default consine_lanczos size
+    default_cl_size= int(1.25*2.0/cf)
+    for (unit_impulse_response,label) in [(cosine_lanczos(unit_impulse,cutoff_period="40h",filter_len=cl_size1),"C_L,size=%s hours"%cl_size_hours1),\
+                         (cosine_lanczos(unit_impulse,cutoff_period="40h"),"C_L,size=%s default"%default_cl_size)]: 
+        b=unit_impulse_response.fillna(0.0)
+        w,h =freqz(b.values,worN=worN)
+        pw=w[1:]
+        ## convert frequence to period in hours
+        period=1.0/pw
+        period=2.0*np.pi*period*dt_hours_ratio
+        hh=np.abs(h)
+        ax.plot(period,hh[1:],linewidth=1,label=label)   
+    ax.set_xlim(0.1,400)
+    ax.axvline(x=cutoff_period,ymin=-0.2,linewidth=1,color='r')
+    ax.annotate("cutoff period=%f h"%cutoff_period,(cutoff_period,1.2),xycoords='data',\
+                xytext=(50, 50), textcoords='offset points',\
+               arrowprops=dict(arrowstyle="->"))
+    ax.set_ylabel(r'Magnitude')
+    ax.set_xlabel(r'Period(hours)')
+    plt.grid(b=True, which='both', color='0.9', linestyle='-', linewidth=0.5)
+    plt.tight_layout()
+    ax.legend(loc="lower right")
+    
 if __name__=="__main__":
     ## compare response for data with 15min interval 
-    compare_response(40)
+    unit_impulse=unit_impulse_ts(5001,"15min")
+    ## 
+    compare_response2(unit_impulse,40)
+    #compare_response(40)
     plt.savefig('frequency_response',bbox_inches=0)
     
