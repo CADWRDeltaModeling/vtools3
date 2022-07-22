ab1dfc0ffbc552de52c569d0cbc5da17abc290f0
Eli <Eli.Ateljevich@water.ca.gov>
2022-07-21 21:32:24 -0700

    Update docs

--- docs/html/_static/documentation_options.js
+++ docs/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.6.3+5.g9078183.dirty',
+    VERSION: '3.6.3+13.g0549812.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
