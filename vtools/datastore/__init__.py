44285f075c7482f9f76659add4810a74b4536880
Eli <Eli.Ateljevich@water.ca.gov>
2022-10-10 18:23:26 -0700

    Removed most of /datastore to dms_datastore.

--- vtools/datastore/__init__.py
+++ vtools/datastore/__init__.py
@@ -1,5 +1,4 @@
-from .download_nwis import nwis_download
-from .download_noaa import noaa_download
-from .download_wdl import wdl_download
-from .download_cdec import cdec_download
-from .read_ts import read_ts
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except ImportError:
+    pass
