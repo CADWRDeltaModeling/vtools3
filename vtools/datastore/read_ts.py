b2e6e12ac20c1ae461c1bdafb57d9314b8d10441
Eli <Eli.Ateljevich@water.ca.gov>
2022-12-04 20:26:00 -0800

    Revised import

--- vtools/datastore/read_ts.py
+++ vtools/datastore/read_ts.py
@@ -4,6 +4,6 @@ all = ["read_ts"]
 
 def read_ts(fpath, start=None, end=None, force_regular=True,nrows=None, selector = None,hint=None):
     warnings.warn("Moved to the dms_datastore package." , PendingDeprecationWarning)
-    import dms_datastore
-    dms_datastore.read_ts(fpath=fpath,start=start,end=end,force_regular=force_regular,nrows=nrows,selector=selector,hint=hint)
+    import dms_datastore.read_ts
+    dms_datastore.read_ts.read_ts(fpath=fpath,start=start,end=end,force_regular=force_regular,nrows=nrows,selector=selector,hint=hint)
 
