edb4fad71d1ebffc8831c35c27aaae5f2a4476a6
Kijin Nam <knam@water.ca.gov>
2024-02-26 16:05:18 -0800

    Update versioneer
    VTOOL-11

--- vtools/__init__.py
+++ vtools/__init__.py
@@ -4,9 +4,8 @@ __author__ = """Eli Ateljevich, Nicky Sandhu, Kijin Nam"""
 __email__ = 'eli@water.ca.gov'
 __version__ = '3.0.4'
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
 
 from vtools.data.gap import *
 from vtools.data.vtime import *
