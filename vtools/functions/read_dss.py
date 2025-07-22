e4d064e9acb4a6eb3d149f97e78ed248abdc00e3
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2025-07-22 14:59:14 -0700

    allow for multiple paths to be passed to read_dss

--- vtools/functions/read_dss.py
+++ vtools/functions/read_dss.py
@@ -47,51 +47,57 @@ def read_dss(
     ----------
     filename: str|Path
         Path to the DSS file to read
-    pathname: str
-        Pathname within the DSS file to read.
+    pathname: str|list
+        Pathname(s) within the DSS file to read.
         Needs to be in the format '/A_PART/B_PART/C_PART/D_PART/E_PART/F_PART/'
         (e.g. '//RSAN112/FLOW////')
     """
     ts_out_list = []
     col_names = []
-    print(f"\tReading pathname: {pathname}")
-    if len(pathname.split("/")[1:-1]) != 6:
-        raise ValueError(f"Invalid DSS pathname: {pathname}, needs 6 parts (A-F)")
-    ts = get_ts(str(filename), pathname)
-    for i, tsi in enumerate(ts):
-        ts_path = tsi[0].columns.values[0]
-        if exclude_pathname is None or (
-            exclude_pathname is not None
-            and not check_exclude(ts_path, exclude_pathname)
-        ):
-            # if not an excluded path, then carry on
-            path_lst = (ts_path).split("/")
-            path_e = path_lst[5]
-            # Set default start_date and end_date to cover the full period of record if not specified
-            tt_full = tsi[0]
-            if start_date is None:
-                start_date = tt_full.index[0].to_timestamp()
-            if end_date is None:
-                end_date = tt_full.index[-1].to_timestamp()
-            if tt_full.index[0].to_timestamp() > end_date or (
-                tt_full.index[-1].to_timestamp() < start_date
+    if isinstance(pathname, str):
+        pathname = [pathname]
+    for path in pathname:
+        if len(path.split("/")[1:-1]) != 6:
+            raise ValueError(f"Invalid DSS path: {path}, needs 6 parts (A-F)")
+    ts = get_ts(str(filename), *pathname)
+    for path in pathname:
+        print(f"\tReading path: {path}")
+        for i, tsi in enumerate(ts):
+            ts_path = tsi[0].columns.values[0]
+            if exclude_pathname is None or (
+                exclude_pathname is not None
+                and not check_exclude(ts_path, exclude_pathname)
             ):
-                raise ValueError(
-                    f"File: {filename} does not cover the dates requested. \n\tRequested dates are: {start_date} to {end_date}, \n\tand the file covers {tt_full.index[0]} to {tt_full.index[-1]}"
+                # if not an excluded path, then carry on
+                path_lst = (ts_path).split("/")
+                path_e = path_lst[5]
+                # Set default start_date and end_date to cover the full period of record if not specified
+                tt_full = tsi[0]
+                if start_date is None:
+                    start_date = tt_full.index[0].to_timestamp()
+                if end_date is None:
+                    end_date = tt_full.index[-1].to_timestamp()
+                if tt_full.index[0].to_timestamp() > end_date or (
+                    tt_full.index[-1].to_timestamp() < start_date
+                ):
+                    raise ValueError(
+                        f"File: {filename} does not cover the dates requested. \n\tRequested dates are: {start_date} to {end_date}, \n\tand the file covers {tt_full.index[0]} to {tt_full.index[-1]}"
+                    )
+                tt = tt_full[start_date:end_date]
+                pidx = pd.period_range(
+                    tt.index[0], tt.index[-1], freq=dss_e2_freq[path_e]
                 )
-            tt = tt_full[start_date:end_date]
-            pidx = pd.period_range(tt.index[0], tt.index[-1], freq=dss_e2_freq[path_e])
-            ptt = pd.DataFrame(tt.values[:, 0], pidx)
+                ptt = pd.DataFrame(tt.values[:, 0], pidx)
 
-            # Interpolate with rhistinterp
-            if p > 0:
-                col_data = rhistinterp(ptt, dt, p=p)
-            elif p == 0:
-                col_data = rhistinterp(ptt, dt)
-            else:
-                col_data = tsi[0]
-            ts_out_list.append(col_data)
-            col_names.append(ts_path)
+                # Interpolate with rhistinterp
+                if p > 0:
+                    col_data = rhistinterp(ptt, dt, p=p)
+                elif p == 0:
+                    col_data = rhistinterp(ptt, dt)
+                else:
+                    col_data = tsi[0]
+                ts_out_list.append(col_data)
+                col_names.append(ts_path)
 
     if ts_out_list:
         ts_out = pd.concat(ts_out_list, axis=1)
@@ -101,7 +107,7 @@ def read_dss(
         with DSSFile(filename) as dssh:
             dfcat = dssh.read_catalog()
         raise ValueError(
-            f"Warning: DSS data not found for {pathname}. Preview of available paths in {filename} are: {dfcat}"
+            f"Warning: DSS data not found for {path}. Preview of available paths in {filename} are: {dfcat}"
         )
 
     return ts_out
