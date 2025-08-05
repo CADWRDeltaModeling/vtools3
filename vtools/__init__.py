9d3bbe84cdcb30399d0dbd64caf5f332c2f48c22
lily-tomkovic-DWR <Lily.Tomkovic@water.ca.gov>
2025-08-05 14:28:35 -0700

    version should look for vtools3 not vtools

--- vtools/__init__.py
+++ vtools/__init__.py
@@ -6,7 +6,7 @@ __email__ = "Eli.Ateljevich@water.ca.gov; Kijin.Nam@water.ca.gov"
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version("vtools")
+    __version__ = version("vtools3")
 except PackageNotFoundError:
     __version__ = "0.0.0"  # Default version if package metadata is unavailable
 
